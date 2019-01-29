a2c72e04604e57f2225fbf938dca884ba601eca6
Yemeda Grayson <42628679+YoitsMeda@users.noreply.github.com>
2019-01-28 19:48:23 -0600

    Add files via upload
    Final Product

--- techjobs-oo-master/src/main/java/org/launchcode/controllers/JobController.java
+++ techjobs-oo-master/src/main/java/org/launchcode/controllers/JobController.java
@@ -1,10 +1,15 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.forms.JobForm;
+import org.launchcode.models.CoreCompetency;
+import org.launchcode.models.Employer;
 import org.launchcode.models.Job;
+import org.launchcode.models.Location;
+import org.launchcode.models.PositionType;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -49,11 +54,34 @@ public class JobController
     		/*model.addAttribute(JobForm);*/
             return "new-job";
     	}
+    	else
+    	{
+    		Job job = new Job();
+
+    		Employer employer = jobData.getEmployers().findById(jobForm.getEmployerId());
+    		if(jobForm.getEmployers() != null) job.setEmployer(employer);
+
+    		Location location = jobData.getLocations().findById(jobForm.getLocationId());
+    		if(jobForm.getLocations() != null) job.setLocation(location);
+
+    		if(jobForm.getPositionTypes() != null)
+
+    		{
+	    		PositionType positionType = jobData.getPositionTypes().findById(jobForm.getPositionTypeId());
+	    		job.setPositionType(positionType);
+    		}
+    		CoreCompetency coreCompetency = jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId());
+    		if(jobForm.getCoreCompetencies() != null)job.setCoreCompetency(coreCompetency);
+
+    		if(jobForm.getName() != null) job.setName(jobForm.getName());
+
+    		jobData.add(job);
+    		String view = "redirect:/job?id=" + job.getId();
+			return view;
+    	}
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
-
-        return "";
 
     }
 }
